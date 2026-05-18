10dc8d7798f1b758cc80fc4bf3d73d67796a0f66
thelaxmangiri <team.laxmang@gmail.com>
2026-05-18 18:19:12 +0545

    Add files via upload

---
+++ firebase-config.example.js
@@ -0,0 +1,17 @@
+// firebase-config.example.js
+// Copy this file to firebase-config.js and fill in your Firebase & Apps Script deployment details.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3HbvK36FRRKk88q4YvdbIHZ6Lw9Alcgw",
+  authDomain: "the-running-banker.firebaseapp.com",
+  projectId: "the-running-banker",
+  storageBucket: "the-running-banker.firebasestorage.app",
+  messagingSenderId: "611675400249",
+  appId: "1:611675400249:web:8d1eca3ca7b6853ae005e8"
+};
+
+// Authorized administrator email who can access the dashboard.
+const AUTHORIZED_ADMIN_EMAIL_CONFIG = "team.laxmang@gmail.com";
+
+// Deployed Google Apps Script execution URL.
+const APPS_SCRIPT_URL_CONFIG = "https://script.google.com/macros/s/AKfycbxsBvg7ZGT-wpQmwAb7zbdmPXL4ZeXp61kKfwGOOf6AMdDi-59eqUikenM0a5qddRUxLQ/exec";
---
+++ firebase-config.js
@@ -0,0 +1,16 @@
+// firebase-config.js
+// Fill in your Firebase & Google Apps Script details here.
+// Currently running in Demonstration Mode.
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA3HbvK36FRRKk88q4YvdbIHZ6Lw9Alcgw",
+  authDomain: "the-running-banker.firebaseapp.com",
+  projectId: "the-running-banker",
+  storageBucket: "the-running-banker.firebasestorage.app",
+  messagingSenderId: "611675400249",
+  appId: "1:611675400249:web:8d1eca3ca7b6853ae005e8"
+};
+
+const AUTHORIZED_ADMIN_EMAIL_CONFIG = "team.laxmang@gmail.com";
+
+const APPS_SCRIPT_URL_CONFIG = "https://script.google.com/macros/s/AKfycbxsBvg7ZGT-wpQmwAb7zbdmPXL4ZeXp61kKfwGOOf6AMdDi-59eqUikenM0a5qddRUxLQ/exec";
